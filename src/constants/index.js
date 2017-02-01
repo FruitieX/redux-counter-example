a1eeb10a4480a41117677abae22a29a98e35d196
Rasmus Eskola <fruitiex@gmail.com>
2017-02-01 15:32:13 +0200

    Delete unused files

--- src/constants/index.js
+++
@@ -1,2 +0,0 @@
-export * as urls from './urls'
-export * as actionTypes from './actionTypes'
--- src/constants/urls.js
+++
@@ -1,5 +0,0 @@
-const API = 'https://react.didierfranc.com'
-
-export const SIGNUP = `${API}/signup`
-export const LOGIN = `${API}/login`
-export const LOGIN_WITH_TOKEN = `${API}/token`
