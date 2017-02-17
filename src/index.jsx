a1eeef1c59b49f769bfa1044b3a405ebb66c7b7b
Rasmus Eskola <fruitiex@gmail.com>
2017-02-17 09:59:18 +0200

    Clarify offline-plugin updates code

--- src/index.jsx
+++ src/index.jsx
@@ -37,26 +37,29 @@ try {
   // otherwise we break hot reloading
 }
 
+// offline-plugin: Apply updates immediately
+// https://github.com/NekR/offline-plugin/blob/master/docs/updates.md
 if (process.env.NODE_ENV === 'production') {
   Offline.install({
     onUpdating: () => {
-      console.log('SW Event:', 'onUpdating');
+      // console.log('SW Event:', 'onUpdating');
     },
 
     onUpdateReady: () => {
-      console.log('SW Event:', 'onUpdateReady');
+      // console.log('SW Event:', 'onUpdateReady');
       // Tells to new SW to take control immediately
       Offline.applyUpdate();
     },
 
     onUpdated: () => {
-      console.log('SW Event:', 'onUpdated');
+      // console.log('SW Event:', 'onUpdated');
       // Reload the webpage to load into the new version
       window.location.reload();
     },
 
     onUpdateFailed: () => {
-      console.log('SW Event:', 'onUpdateFailed');
+      // TODO: alert user
+      // console.log('SW Event:', 'onUpdateFailed');
     },
   });
 }
