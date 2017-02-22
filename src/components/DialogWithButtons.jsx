e9fe7cbd841d74431ea03a3196661ad679fffd4a
Rasmus Eskola <fruitiex@gmail.com>
2017-02-22 22:01:52 +0200

    <DialogWithButtons> supports showing loading spinner

--- src/components/DialogWithButtons.jsx
+++ src/components/DialogWithButtons.jsx
@@ -3,8 +3,32 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import RefreshIndicator from 'material-ui/RefreshIndicator';
 
 import ImageUpload from '../components/ImageUpload';
+
+const styles = {
+  container: {
+    position: 'relative',
+  },
+  fadeContainer: {
+    opacity: 0,
+  },
+  opaqueContainer: {
+    opacity: 1,
+    maxHeight: 200,
+    transition: 'all .5s',
+  },
+  refresh: {
+    marginLeft: '50%',
+    marginTop: 20,
+  },
+  refreshContainer: {
+    flex: 1,
+    textAlign: 'center',
+    padding: 20,
+  },
+};
 
 class DialogWithButtons extends React.Component {
   constructor(props) {
@@ -48,6 +72,7 @@ class DialogWithButtons extends React.Component {
       isOpen,
       description,
       textField,
+      loading,
     } = this.props;
 
     const actions = [];
@@ -74,6 +99,48 @@ class DialogWithButtons extends React.Component {
       />,
     );
 
+    const dialogContents = (
+      <div style={styles.container}>
+        <RefreshIndicator style={styles.refresh} size={40} top={0} left={-20} status={loading ? 'loading' : 'hide'} />
+
+        <div style={loading ? styles.fadeContainer : styles.opaqueContainer}>
+          <div>
+            { description }
+          </div>
+
+          { textField ?
+            <div>
+              <TextField
+                floatingLabelText={textField.label}
+                value={this.state.value}
+                onChange={this.handleChange}
+                autoFocus
+                onKeyDown={this.keyDown}
+              />
+            </div>
+            :
+            null
+          }
+
+          <p>
+            { textField && textField.textAfter }
+          </p>
+
+          { imageUpload ?
+            <div>
+              <ImageUpload setImageUrl={this.setImageUrl} label={imageUpload.label} />
+            </div>
+            :
+            null
+          }
+
+          <p>
+            { imageUpload && imageUpload.textAfter }
+          </p>
+        </div>
+      </div>
+    );
+
     return (
       <Dialog
         title={title}
@@ -83,40 +150,7 @@ class DialogWithButtons extends React.Component {
         onRequestClose={close}
         autoScrollBodyContent
       >
-        <div>
-          { description }
-        </div>
-
-        { textField ?
-          <div>
-            <TextField
-              floatingLabelText={textField.label}
-              value={this.state.value}
-              onChange={this.handleChange}
-              autoFocus
-              onKeyDown={this.keyDown}
-            />
-          </div>
-          :
-          null
-        }
-
-        <p>
-          { textField && textField.textAfter }
-        </p>
-
-        { imageUpload ?
-          <div>
-            <ImageUpload setImageUrl={this.setImageUrl} label={imageUpload.label} />
-          </div>
-          :
-          null
-        }
-
-        <p>
-          { imageUpload && imageUpload.textAfter }
-        </p>
-
+        { dialogContents }
       </Dialog>
     );
   }
@@ -141,6 +175,7 @@ DialogWithButtons.propTypes = {
   submit: React.PropTypes.func.isRequired,
   close: React.PropTypes.func.isRequired,
   isOpen: React.PropTypes.bool.isRequired,
+  loading: React.PropTypes.bool,
 };
 
 DialogWithButtons.defaultProps = {
@@ -148,6 +183,7 @@ DialogWithButtons.defaultProps = {
   imageUpload: null,
   description: '',
   cancelAction: null,
+  loading: false,
 };
 
 export default DialogWithButtons;
