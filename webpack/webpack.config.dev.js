28c8cda0b5f54537ea8402d93a910c074359cc04
Rasmus Eskola <fruitiex@gmail.com>
2017-05-26 09:54:33 +0300

    Use file-loader on .json files also in dev environments

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -29,7 +29,7 @@ module.exports = {
         use: 'babel-loader',
       },
       {
-        test: /\.(png|jpg)$/,
+        test: /\.(png|jpg|json)$/,
         use: 'file-loader',
       },
     ],
